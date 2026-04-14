f5c74665d50d86cdeaf10cff599578e353256f38
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2026-04-14 11:13:25 +0200

    stranky pro prihl a reg

---
+++ stranky_skoly/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using stranky_skoly.Models;
+using System.Diagnostics;
+
+namespace stranky_skoly.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Přihlášení()
+        {
+            return View();
+        }
+
+        public IActionResult Registrace()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
