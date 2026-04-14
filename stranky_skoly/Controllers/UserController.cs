eafe250ecc58e383ef0d9a137e7341fc04971e35
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2026-04-14 11:29:25 +0200

    disignova uprava prihlaseni a reg
    musi se neco udelat a pridat databaze

--- stranky_skoly/Controllers/UserController.cs
+++ stranky_skoly/Controllers/UserController.cs
@@ -11,9 +11,43 @@ namespace stranky_skoly.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Přihlášení(string jmeno, string heslo)
+        {
+            // Zde by normálně probíhalo ověření přes databázi.
+            // Pro teď uděláme ukázku: Můžete se přihlásit jen jako "admin" s heslem "1234".
+            if (jmeno == "admin" && heslo == "1234")
+            {
+                // Úspěšné přihlášení -> Přesměrujeme uživatele, například na úvodní stránku (Home/Index)
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Pokud je heslo nebo jméno špatné, přidáme chybu do ModelState z vašeho kódu
+            ModelState.AddModelError(string.Empty, "Špatné jméno nebo heslo.");
+
+            // Vrátíme uživatele zpět na přihlašovací formulář, kde se mu následně zobrazí chyba
+            return View();
+        }
+
         public IActionResult Registrace()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrace(string jmeno, string heslo, string heslo2)
+        {
+            // Kontrola, zda se hesla shodují
+            if (heslo != heslo2)
+            {
+                ModelState.AddModelError(string.Empty, "Zadaná hesla se neshodují.");
+                return View();
+            }
+
+            // Zde by normálně proběhlo uložení uživatele do databáze
+
+            // Po úspěšné registraci přesměrovat třeba na přihlášení
+            return RedirectToAction("Přihlášení");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
