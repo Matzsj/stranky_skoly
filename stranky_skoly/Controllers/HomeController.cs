4194ab5000e0259a891ae928eabfb18a120ab532
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2026-04-14 10:09:56 +0200

    komit 1

---
+++ stranky_skoly/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using stranky_skoly.Models;
+using System.Diagnostics;
+
+namespace stranky_skoly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
